481ba1d3b57148ff21068edab1ecce8567f560f0
sngw lee <tkddn2932@gmail.com>
2023-12-06 11:18:43 +0900

    add host0000

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import pandas as pd
 #df = pd.read_csv('D:\\Sangwoo\\00.자기관리_인생계획\\자기관리_인생계획\\2.부동산공부\\00.연구자료\\data\\meme_all_data.csv')
 df = pd.read_csv('/home/ubuntu/source_data/meme_all_data.csv')
 
-
 df = df.astype({'price':'int64'})
 year_aixs = [y for y in range(2006,2023)]
 
@@ -223,4 +222,4 @@ def update_datatable(dong,gu,year,city):
     
 #Step6 Run App
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8050,debug=True)
